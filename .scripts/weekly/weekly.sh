630d0d32acad36c7b499f17451655b2eebeb9e3c
emilrwx <emil.nuutinen@gmail.com>
2020-11-22 13:26:56 +0200

    Create weekly.sh

---
+++ .scripts/weekly/weekly.sh
@@ -0,0 +1,22 @@
+#!/bin/sh
+
+# filename
+filename=week_$(date -d "last week" "+%V").md
+
+# current year
+year=$(date "+%Y")
+
+# whole path
+path=~/notes/personal/weekly/$year/$filename
+
+# file extension
+extension="${filename##*.}"
+
+if [ "$extension" == "md" ]; then
+   cp -n ~/.scripts/weekly/weekly_template.md $path;
+else
+  echo "Template not found"
+  exit
+fi
+
+vim $path
