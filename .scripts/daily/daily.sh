4007f5414f0ddf617075143927d4df7da3ea850b
emilrwx <emil.nuutinen@gmail.com>
2020-11-26 00:11:03 +0200

    Update daily.sh

--- .scripts/daily/daily.sh
+++ .scripts/daily/daily.sh
@@ -6,17 +6,11 @@ filename=$(date "+%d_%m_%Y").md
 # current month_year
 month=$(date "+%m_%Y")
 
-# whole path
-path=~/notes/personal/daily/$month/$filename
+# whole path to file
+file=~/notes/personal/daily/$month/$filename
 
-# file extension
-extension="${filename##*.}"
-
-if [ "$extension" == "md" ]; then
-   cp -n ~/.scripts/daily/daily_template.md $path;
+if [ -f "$file" ]; then
+    vim $file;
 else
-  echo "Template not found"
-  exit
+    cp -n ~/.scripts/daily/daily_template.md $file; vim $file;
 fi
-
-vim $path
