52e69ba7fa7e7178ef9642171af28477ef223225
emilrwx <emil.nuutinen@gmail.com>
2021-01-03 13:48:06 +0200

    Update journal.sh

--- .scripts/journal/journal.sh
+++ .scripts/journal/journal.sh
@@ -6,6 +6,9 @@ filename=week_$(date "+%V").md
 # current year
 year=$(date "+%Y")
 
+# year last monday (if year changes mid week)
+year_last_monday=$(date -d "last monday" +"%Y")
+
 # get this weeks weekdays and their dates
 mon=$(date --date="today" +"%a %d-%m-%Y")
 tue=$(date --date="next Tuesday" +"%a %d-%m-%Y")
@@ -16,10 +19,20 @@ sat=$(date --date="next Saturday" +"%a %d-%m-%Y")
 sun=$(date --date="next Sunday" +"%a %d-%m-%Y")
 
 # whole path to file
-file=~/notes/journal/$year/$filename
+path=~/notes/journal/$year/$filename
 
-if [ -f "$file" ]; then
-    vim $file;
-else
-    echo -e "## $mon\n\n\n\n## $tue\n\n\n\n## $wed\n\n\n\n## $thu\n\n\n\n## $fri\n\n\n\n## $sat\n\n\n\n## $sun\n\n\n" >> $file; vim $file;
+# whole path to file if year changed mid week
+path_last_monday=~/notes/journal/$year_last_monday/$filename
+
+# current weekday
+today=$(date +"%A")
+
+if [ $today == "Monday" ]; then
+    if [ -f "$path" ]; then
+        vim $path;
+    else
+        echo -e "## $mon\n\n\n\n## $tue\n\n\n\n## $wed\n\n\n\n## $thu\n\n\n\n## $fri\n\n\n\n## $sat\n\n\n\n## $sun\n\n\n" >> $path; vim $path;
+    fi
+elif [ $today != "Monday" ]; then
+    vim $path_last_monday;
 fi
